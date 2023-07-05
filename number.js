5575dbdd4bdcd5d73543438b50b8331f105b54c1
Sanjeet Prajapati <prajapatisanjeet50@gmail.com>
2023-07-05 20:01:16 +0530

    working now just have to make some attractive

--- number.js
+++ number.js
@@ -1,9 +1,24 @@
-function result(){
+let count = 1
+temp = Math.floor(Math.random() * 11);
+function result() {
     let a = document.getElementById("num")
-    if (a.value == 5) {
-        document.getElementById("result").innerHTML = "yes"
+    // document.getElementById("tp").innerHTML = temp
+
+    if (a.value < temp) {
+        count = count + 1;
+        document.getElementById("result").innerHTML = "Higher Number please "
+        document.getElementById("result").style.backgroundColor = "coral"
+        
     }
-    else {
-        document.getElementById("result").innerHTML = "np"
+    if (a.value > temp) {
+        count = count + 1
+        document.getElementById("result").innerHTML = "Lower Number please "
+        document.getElementById("result").style.backgroundColor = "coral"
+
+    }
+    if (a.value == temp) {
+        document.getElementById("result").style.backgroundColor = "chartreuse";
+    
+        document.getElementById("result").innerHTML = "you r correct in " + count + " attempts";
     }
 }
